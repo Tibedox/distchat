ba87d2ed917bfa40f0a1345a7c4d69feaff428af
Tibedox <tibedox@mail.ru>
2023-04-19 23:27:45 +0600

    version 1.0

---
+++ app/src/main/java/ru/myitschool/distchat/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.myitschool.distchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
