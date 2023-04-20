77b9b6fcfb58be39a49d79f15dd648d660b006d6
Tibedox <tibedox@mail.ru>
2023-04-20 22:50:12 +0600

    version 1.0

--- app/src/main/java/ru/myitschool/distchat/MainActivity.java
+++ app/src/main/java/ru/myitschool/distchat/MainActivity.java
@@ -3,12 +3,24 @@ package ru.myitschool.distchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText1;
+    EditText editText2;
+    TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText1 = findViewById(R.id.edit1);
+        editText2 = findViewById(R.id.edit2);
+        textView = findViewById(R.id.text1);
+        button = findViewById(R.id.button);
     }
 }
