6c0406bf517d3e94b01823c322d06a39e8b29243
Tibedox <tibedox@mail.ru>
2023-04-26 23:02:33 +0600

    version 1.2

--- app/src/main/java/ru/myitschool/distchat/MainActivity.java
+++ app/src/main/java/ru/myitschool/distchat/MainActivity.java
@@ -3,10 +3,14 @@ package ru.myitschool.distchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editText2;
     TextView textView;
     Button button;
+    MyData myData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +36,25 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         MyApi myApi = retrofit.create(MyApi.class);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt(editText1.getText().toString());
+                int y = Integer.parseInt(editText2.getText().toString());
+                myApi.getData(x, y).enqueue(new Callback<MyData>() {
+                    @Override
+                    public void onResponse(Call<MyData> call, Response<MyData> response) {
+                        myData = response.body();
+                        textView.setText("x0+y="+myData.z);
+                    }
+
+                    @Override
+                    public void onFailure(Call<MyData> call, Throwable t) {
+                        textView.setText("не вышло");
+                    }
+                });
+            }
+        });
     }
 }
